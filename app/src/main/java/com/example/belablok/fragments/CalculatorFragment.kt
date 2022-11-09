fcf2af9cacc8c7701d527c8729ac87190df68662
Filip Begić <filip.begic@gdi.net>
2022-11-09 16:06:15 +0100

    Initial commit. ToRemove: - main activity methods

---
+++ app/src/main/java/com/example/belablok/fragments/CalculatorFragment.kt
@@ -0,0 +1,18 @@
+package com.example.belablok.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.belablok.R
+
+class CalculatorFragment : Fragment(R.layout.fragment_calculator) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button1).setOnClickListener {
+            view.findViewById<Button>(R.id.button2).text = "Click"
+        }
+    }
+}
