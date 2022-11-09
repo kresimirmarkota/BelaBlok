89f1e5c68b55ec8e74e1f91b567b29409a3012fc
Filip Begić <filip.begic@gdi.net>
2022-11-09 16:05:25 +0100

    Initial commit. ToRemove: - main activity methods

--- app/src/main/java/com/example/belablok/MainActivity.kt
+++ app/src/main/java/com/example/belablok/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.belablok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.belablok.fragments.CalculatorFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, CalculatorFragment())
+            .commit()
     }
 }
